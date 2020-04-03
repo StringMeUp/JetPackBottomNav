91cf9ab4e2359a629a763b152ce25362600c7ec9
Samir Ramic <ramic.samirr@gmail.com>
2020-04-03 14:09:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/jetpackbottomnav/FragmentAccount.kt
@@ -0,0 +1,25 @@
+package com.example.jetpackbottomnav
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FragmentAccount : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/jetpackbottomnav/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.jetpackbottomnav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController : NavController
+    private lateinit var bottomNavView : BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment_Id)
+        bottomNavView = bottomNavigationView_Id
+        NavigationUI.setupWithNavController(bottomNavView, navController)
+    }
+}
